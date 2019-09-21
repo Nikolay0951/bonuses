22d8171b0f23baba6830eaf8ff9243b38bd092bb
Blalala <student>
2019-09-21 11:32:04 +0300

    Рефакторинг: тернарный оератор

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ console.log('worked');
 const purchaseAmount = 1000;
 const availableBonuses = 100;
 
-const maxBonuses = purchaseAmount * 0.5;
-let applieBonuses;
-if (maxBonuses <= availableBonuses) {
-    applieBonuses = maxBonuses;
-} else {
-    applieBonuses =availableBonuses;
-} 
+ const maxBonuses = purchaseAmount * 0.5; 
+
+const applieBonuses = maxBonuses <= availableBonuses ? maxBonuses : availableBonuses; // тернарный оператор
 
 console.log(applieBonuses)
 const total = purchaseAmount - applieBonuses;
